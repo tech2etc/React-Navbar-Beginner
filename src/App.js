0224cb42ee36fda5410d843c21551d46951455e3
Tech2 etc <info@tech2etc.com>
2022-08-08 19:25:58 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import "./HomeStyles.css";
+
+function Home() {
+  return (
+    <>
+      <div className="home">
+        <h1>React Navbar</h1>
+        <h3>Difficulty Level: 0;</h3>
+      </div>
+    </>
+  );
+}
+
+export default Home;
